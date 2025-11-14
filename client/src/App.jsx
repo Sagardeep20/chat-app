a2042874973ff284949e68434cec71119bffd6e1
Sagardeep Choudhury <sagardeepchoudhury01@gmail.com>
2025-11-15 00:22:49 +0530

    fixed bgImage using import (works on vercel)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,44 @@
 import { useContext } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
-import {Toaster} from "react-hot-toast"
-import { AuthContext } from '../context/AuthContext';
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { AuthContext } from "../context/AuthContext";
 
+import bg from "./assets/bgImage.svg"; 
 
 const App = () => {
-
-  const {authUser} = useContext(AuthContext)
+  const { authUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] 
-      bg-contain
-      ">
-        <Toaster/>
+    <div
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Toaster />
       <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
